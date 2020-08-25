31cfce7d4c7ff6e02b4cd701d27a8b8035616ad7
aflores <arcyiflores@gmail.com>
2020-08-24 23:42:53 -0400

    Updates

---
+++ precache-manifest.e0031a66dd97c51ed875b4769f64549f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d21f796a940c077bd62dcc381b4b2e",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "84fb253900a813140886",
+    "url": "/resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "cc36b045786f297c6eea",
+    "url": "/resume/static/js/2.c08e30cc.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/resume/static/js/2.c08e30cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84fb253900a813140886",
+    "url": "/resume/static/js/main.4e46ed9f.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  }
+]);
